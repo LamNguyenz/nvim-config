fb7814e9cb7c2f9d9ba44d934a2b7802e58a860c
Lam Nguyen <lam.nguyen@pascalsoftware.com>
2025-09-10 04:45:50 +0700

    Update config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+
+-- K -> run command :lineBreakInsert
+vim.api.nvim_set_keymap("n", "K", ":lineBreakInsert<CR>", { noremap = true, silent = true })
+
+-- L -> go to end of line ($)
+vim.api.nvim_set_keymap("n", "L", "$", { noremap = true, silent = true })
+
+-- H -> go to beginning of line (^)
+vim.api.nvim_set_keymap("n", "H", "^", { noremap = true, silent = true })
